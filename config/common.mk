251419bb283fe8048455f206fa3e29f26a928d37
Raka Prasetya Pratama <123168129+Hirozuto@users.noreply.github.com>
2023-01-22 18:31:05 +0800

    yuki: fix missing endif

--- config/common.mk
+++ config/common.mk
@@ -192,3 +192,4 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+endif
