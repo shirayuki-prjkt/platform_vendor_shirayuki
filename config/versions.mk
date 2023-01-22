9787de3dcc2b189339d6c48e8f6da4c068694b76
Raka Prasetya Pratama <123168129+Hirozuto@users.noreply.github.com>
2023-01-22 17:12:24 +0800

    yuki: adding Maintainer info

--- config/versions.mk
+++ config/versions.mk
@@ -5,6 +5,10 @@ ifndef SHIRAYUKI_BUILD_TYPE
     SHIRAYUKI_BUILD_TYPE := UNOFFICIAL
 endif
 
+ifndef SHIRAYUKI_MAINTAINER
+    SHIRAYUKI_MAINTAINER := Unofficial Maintainer? Yuki cannot recognize the maintainer
+endif
+
 # Set all versions
 SHIRAYUKI_DATE := $(shell date -u +%Y%m%d-%H%M)
 TARGET_BACON_NAME := $(TARGET_PRODUCT)-$(SHIRAYUKI_VERSION)-$(SHIRAYUKI_DATE)-$(SHIRAYUKI_BUILD_TYPE)
@@ -15,5 +19,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.shirayuki.version=$(SHIRAYUKI_VERSION) \
     com.shirayuki.fingerpring=$(SHIRAYUKI_FINGERPRINT) \
     ro.shirayuki.display.version=Shirayuki-$(SHIRAYUKI_VERSION) \
+    ro.shirayuki.maintainer=$(SHIRAYUKI_MAINTAINER) \
     ro.shirayuki.releasetype=$(SHIRAYUKI_BUILD_TYPE) \
     ro.modversion=$(TARGET_BACON_NAME)
